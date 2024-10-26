ee706e9f37fbd6c96550e56d61d1eee8cb89b74b
xorms571 <xorms6865@naver.com>
2024-10-26 23:51:25 +0900

    news add

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+const CLIENT_ID = "SiFXdkNpyPZc309ZLHYX"; // 네이버 클라이언트 ID 입력
+const CLIENT_SECRET = "bjij2iYoVh"; // 네이버 클라이언트 Secret 입력
+
 app.get("/api/search", async (req, res) => {
   const query = req.query.query || "쇼핑";
   const page = parseInt(req.query.page) || 1;
-  const display = parseInt(req.query.display) || 50;
-
+  const display = parseInt(req.query.display) || 100;
   const url = `https://openapi.naver.com/v1/search/shop.json?query=${encodeURIComponent(
     query
   )}&display=${display}&start=${(page - 1) * display + 1}&sort=sim`;
-
   try {
     const response = await axios.get(url, {
       headers: {
-        "X-Naver-Client-Id": 'SiFXdkNpyPZc309ZLHYX',
-        "X-Naver-Client-Secret": 'bjij2iYoVh',
+        "X-Naver-Client-Id": CLIENT_ID,
+        "X-Naver-Client-Secret": CLIENT_SECRET,
       },
     });
-
-    // 가격 제한 없이 데이터를 그대로 클라이언트에 보냄
     res.json({ items: response.data.items });
   } catch (error) {
     console.error("Error fetching data from Naver API:", error);
@@ -33,7 +31,27 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
+app.get('/api/news', async (req, res) => {
+  const query = req.query.q || '뉴스'; 
+  const page = parseInt(req.query.page) || 1;
+  const display = parseInt(req.query.display) || 100;
+  const url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURIComponent(
+    query
+  )}&display=${display}&start=${(page - 1) * display + 1}&sort=sim`;
 
+  try {
+      const response = await axios.get(url, {
+          headers: {
+              'X-Naver-Client-Id': CLIENT_ID,
+              'X-Naver-Client-Secret': CLIENT_SECRET,
+          }
+      });
+      res.json({ items: response.data.items });
+  } catch (error) {
+      console.error("Error fetching news:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 app.listen(PORT, () => {
-    console.log(`서버 실행 http://localhost:${PORT}`);
+  console.log(`서버 실행 http://localhost:${PORT}`);
 });
